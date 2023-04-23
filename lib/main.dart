3659c63f84605746030b0e325f8a296f2e971a87
Gabriel Bonil da Silva <gabrielbonil32@gmail.com>
2023-04-23 15:36:06 -0300

    Sidebar and GridView.count

--- lib/main.dart
+++ lib/main.dart
@@ -41,25 +41,116 @@ class _AppState extends State<App> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Card(
+          elevation: 0,
+          shape: const RoundedRectangleBorder(
+            side: BorderSide(
+              color: Colors.grey,
+            ),
+            borderRadius: BorderRadius.all(Radius.circular(12)),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Row(
+              children: [
+                const Padding(padding: EdgeInsets.only(left: 8)),
+                const Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        border: InputBorder.none,
+                        hintText: "Pesquise o nome ou código do produtos"),
+                  ),
+                ),
+                const Padding(padding: EdgeInsets.symmetric(horizontal: 8)),
+                Icon(Icons.qr_code_rounded, color: Colors.grey.shade600),
+                const Padding(padding: EdgeInsets.only(right: 8)),
+              ],
+            ),
+          ),
+        ),
+      ),
+      drawer: Drawer(
+        child: SingleChildScrollView(
+          child: Column(
+            //Se não achar que precisa disso, da para tirar o Container
+            children: [
+              //Header
+              Container(
+                color: Colors.red,
+                padding: EdgeInsets.only(
+                  top: AppBar().preferredSize.height,
+                ),
+              ),
+
+              //Itens SideBar
+              Wrap(
+                runSpacing: 16, //Espaçamento na vertical
+                // crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  //Header
+                  Container(
+                    color: Colors.red,
+                    padding: EdgeInsets.only(
+                      top: MediaQuery.of(context).padding.top,
+                    ),
+                    child: Column(children: const []),
+                  ),
+                  //Itens do Menu
+                  ListTile(
+                    leading: const Icon(Icons.home_rounded),
+                    title: const Text('Home'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.star),
+                    title: const Text('Example'),
+                    onTap: () {},
+                  ),
+                  //Diviso bonitinho
+                  const Divider(
+                    color: Colors.black,
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.favorite_border_rounded),
+                    title: const Text('Favorite'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.history),
+                    title: const Text('Example2'),
+                    onTap: () {},
+                  ),
+                  ListTile(
+                    leading: const Icon(Icons.access_time),
+                    title: const Text('Example3'),
+                    onTap: () {},
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
       body: Card(
         margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
         child: _pages.elementAt(_selectedIndex),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_rounded),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home_rounded),
-            label: "Home",
-          ),
-        ],
-        currentIndex: _selectedIndex,
-        selectedItemColor: Colors.red.shade900,
-        onTap: _onTap,
-      ),
+      // bottomNavigationBar: BottomNavigationBar(
+      //   items: const [
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home_rounded),
+      //       label: "Home",
+      //     ),
+      //     BottomNavigationBarItem(
+      //       icon: Icon(Icons.home_rounded),
+      //       label: "Home",
+      //     ),
+      //   ],
+      //   currentIndex: _selectedIndex,
+      //   selectedItemColor: Colors.red.shade900,
+      //   onTap: _onTap,
+      // ),
     );
   }
 }
--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -12,38 +12,118 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Card(
-          elevation: 0,
-          shape: const RoundedRectangleBorder(
-            side: BorderSide(
-              color: Colors.grey,
-            ),
-            borderRadius: BorderRadius.all(Radius.circular(12)),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-            child: Row(
-              children: [
-                const Padding(padding: EdgeInsets.only(left: 8)),
-                Icon(Icons.menu, color: Colors.grey.shade600),
-                const Padding(padding: EdgeInsets.only(left: 8)),
-                const Expanded(
-                  child: TextField(
-                    decoration: InputDecoration(
-                        border: InputBorder.none,
-                        hintText: "Pesquise o nome ou código do produtos"),
-                  ),
+        // Fazer a exibição dos itens
+        Expanded(
+          child: GridView.count(
+            crossAxisCount: 2,
+            mainAxisSpacing: 20, // Espaço entre as linhas
+            crossAxisSpacing: 20,
+            children: [
+              Card(
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                        fit: BoxFit.cover,
+                        height: 200),
+                    const Text("Imagem 1"),
+                  ],
                 ),
-                const Padding(padding: EdgeInsets.symmetric(horizontal: 8)),
-                Icon(Icons.qr_code_rounded, color: Colors.grey.shade600),
-                const Padding(padding: EdgeInsets.only(right: 8)),
-              ],
-            ),
+              ),
+
+              Card(
+                child: Column(
+                  children: [
+                    Image.network(
+                        'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+                        fit: BoxFit.cover,
+                        height: 200),
+                    const Text("Imagem 2"),
+                  ],
+                ),
+              ),
+
+              Card(
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl.jpg',
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Column(
+                      children: const [
+                        SizedBox(height: 16), //Substituindo o padding criando isso.
+                        Text(
+                          'Título do Card',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          'Descrição do Card',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+
+              Card(
+                child: Stack(
+                  children: [
+                    Container(
+                      decoration: const BoxDecoration(
+                        image: DecorationImage(
+                          image: NetworkImage(
+                            'https://flutter.github.io/assets-for-api-docs/assets/widgets/owl-2.jpg',
+                          ),
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ),
+                    Positioned(
+                      bottom: 0,
+                      child: Column(
+                        children: const [
+                          Text(
+                            'Título do Card',
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 8), 
+                          Text(
+                            'Descrição do Card',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 8),  //Substituindo o padding criando isso.
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
           ),
         ),
-        Expanded(
-          child: ListView(),
-        )
       ],
     );
   }
